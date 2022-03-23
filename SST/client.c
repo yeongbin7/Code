4d7c4db7dafb0cc4bc5dfb1dfa3002098f9b7879
Yeongbin <89433893+yeongbin7@users.noreply.github.com>
2022-03-23 23:54:19 +0900

    Update client.c

--- SST/client.c
+++ SST/client.c
@@ -22,6 +22,8 @@ long long int: "long long int", unsigned long long int: "unsigned long long int"
       default: "unknown")
 
 #define AUTH_ID_LEN 4
+#define NUMKEY 4
+#define NONCE_SIZE 8
 
 struct topic
 {
@@ -32,9 +34,9 @@ struct topic
 
 struct sessionKeyReq
 {
-    unsigned char Auth_nonce[8] ;
-    unsigned char Entity_nonce[8] ;
-    int *NumKeys ;
+    unsigned char Entity_nonce[NONCE_SIZE] ;
+    unsigned char Auth_nonce[NONCE_SIZE] ;
+    unsigned char NumKeys[4] ;
     char Sender [20] ;
     char Purpose [10] ;
 };
@@ -54,14 +56,33 @@ void purpose()
     strcpy(SessionKeyReq.Purpose , "group");
 
 }
+void numkey()
+{
+    memset(SessionKeyReq.NumKeys,0,sizeof(SessionKeyReq.NumKeys));
+    SessionKeyReq.NumKeys[3] = 1;
+}
 
+void AuthID()
+{
+    for(int i =0 ; i<AUTH_ID_LEN; i++)
+    {       
+        auth_id[i] = message[i+2]; 
+    }
+}
+void AuthNonce()
+{
+    for(int j = 0; j<sizeof(message)-(AUTH_ID_LEN+2)-1;j++)
+    {
+        SessionKeyReq.Auth_nonce[j] = message[AUTH_ID_LEN+2+j];
+    }
+}
 void nonce_generator()  // nonce generator;
 {
-    unsigned char buffer[8];
-    int length = 8;
+    unsigned char buffer[NONCE_SIZE];
+    int length = NONCE_SIZE;
     RAND_bytes(buffer,length);
     printf("Random Entity Number = ");
-    for(int i=0;i<8; i++)
+    for(int i=0;i<NONCE_SIZE; i++)
         {
         printf("%x ", buffer[i]);
         SessionKeyReq.Entity_nonce[i] = buffer[i];
@@ -72,17 +93,24 @@ void nonce_generator()  // nonce generator;
 
 void serializeSessionkeyReq() 
 {
-    *SessionKeyReq.NumKeys = 1;
-    if(SessionKeyReq.Auth_nonce == NULL || SessionKeyReq.Entity_nonce == NULL || 
-    SessionKeyReq.NumKeys == NULL || SessionKeyReq.Purpose == NULL ||  SessionKeyReq.Sender == NULL)
+    unsigned char buf [NONCE_SIZE*2 + NUMKEY]; //buf[20]
+    if(SessionKeyReq.Entity_nonce == NULL || SessionKeyReq.Auth_nonce == NULL || 
+    SessionKeyReq.Sender == NULL || SessionKeyReq.Purpose == NULL ||  SessionKeyReq.NumKeys == NULL)
     {
         printf("Error: SessionKeyReq nonce or replyNonce or purpose or numKeys is missing.");
     }
+    
     else
     {
+        memcpy(buf,SessionKeyReq.Entity_nonce, NONCE_SIZE); //Entity_nonce
+        memcpy(buf+NONCE_SIZE,SessionKeyReq.Auth_nonce,NONCE_SIZE); //Auth_nonce
+        memcpy(buf+NONCE_SIZE*2,SessionKeyReq.NumKeys,NUMKEY); // Key_num 4byte
+        
 
+        for(int i=0; i<sizeof(buf);i++)
+            printf(" %x ", buf[i]);
     }
-    
+    printf("\n");
 }
 
 
@@ -118,34 +146,20 @@ void TcpCommunication(int argc, char* argv[]) // TCP Connection(client)
         printf("%x ",message[i]);
     }
     close(my_sock); //4번
-  
-}
-void AuthID()
-{
-    for(int i =0 ; i<AUTH_ID_LEN; i++)
-    {       
-        auth_id[i] = message[i+2]; 
-    }
-}
-void AuthNonce()
-{
-    for(int j = 0; j<sizeof(message)-(AUTH_ID_LEN+2)-1;j++)
-    {
-        SessionKeyReq.Auth_nonce[j] = message[AUTH_ID_LEN+2+j];
-    }
 }
 int main(int argc, char* argv[])
 {
         TcpCommunication(argc, argv);
         AuthID();
         AuthNonce();
+        numkey();
         printf("\n");
         nonce_generator();
         purpose();
 
         if(message[0] == 0)   //Auth_Hello 받았을 때!
         {
-
+            serializeSessionkeyReq();
         }
             
     
